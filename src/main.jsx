40a69725f7b56a9ba4b5b614173e1e21c25ef17d
Arshia Jenab <Jenab.Dev@gmail.com>
2025-12-07 19:06:49 +0330

    Change BrowserRouter basename from '/personiltytest' to '/colortest/'

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
     <StrictMode>
-        <BrowserRouter basename="/personiltytest">
+        <BrowserRouter basename="/colortest/">
             <App />
         </BrowserRouter>
     </StrictMode>
